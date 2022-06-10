ddabf5baa9894da96f87174af3595330be306bdb
b-reyes <reyesb123@gmail.com>
2022-06-09 17:10:57 -0700

    add preliminary semi-variogram widget

--- EchoPro/semivariogram/semivariogram.py
+++ EchoPro/semivariogram/semivariogram.py
@@ -1,4 +1,7 @@
 import numpy as np
+from scipy import special
+import ipywidgets as widgets
+import matplotlib.pyplot as plt
 import warnings
 import sys
 
@@ -29,7 +32,22 @@ class SemiVariogram:
         self.y = y
         self.field = field
 
-    def calculate_semi_variogram(self, bins):
+        # bins provided to calculate_semi_variogram
+        self._center_bins = None
+
+        # standardized semi-variogram values
+        self.gamma_standardized = None
+
+        # widget variables
+        self._model_drop = None
+        self._sill_box = None
+        self._len_scale_box = None
+        self._exp_pow_box = None
+        self._ls_hole_eff_box = None
+        self._nugget_box = None
+        self._lsq_toggle = None
+
+    def calculate_semi_variogram(self, center_bins):
         """
         Calculates the semi-variogram standardized by the
         standard deviation of the head multiplied by
@@ -47,6 +65,8 @@ class SemiVariogram:
 
         """
 
+        self._center_bins = center_bins # TODO: put bins in ascending order
+
         x_diff = np.subtract.outer(self.x, self.x)
         y_diff = np.subtract.outer(self.y, self.y)
 
@@ -57,12 +77,14 @@ class SemiVariogram:
         dis = np.sqrt(np.power(x_diff, 2) + np.power(y_diff, 2))
 
         # obtain the upper triangular portion of dis
-        dis_2 = np.triu(dis, k=1)  # TODO: we can put this in a sparse form
+        dis_2 = np.triu(dis, k=1)  # TODO: we can put this in a sparse form can remove dis_2 != 0, if implemented
 
-        gamma_standardized = []
-        for i in range(len(bins) - 1):
-            # get indices of distances that are between bins[i] and bins[i+1] (i.e. in the lag)
-            ind_in_lag = np.argwhere((bins[i] <= dis_2) & (dis_2 < bins[i + 1]) & (dis_2 != 0))
+        self.gamma_standardized = []
+        for i in range(len(self._center_bins) - 1):
+            # get indices of distances that in the lag
+            bin_add = (self._center_bins[i + 1] - self._center_bins[i])/2.0
+            ind_in_lag = np.argwhere((self._center_bins[i] - bin_add <= dis_2)
+                                     & (dis_2 < self._center_bins[i + 1] + bin_add) & (dis_2 != 0))
 
             # indices in the lag
             x_ind = ind_in_lag[:, 0]
@@ -75,10 +97,104 @@ class SemiVariogram:
             # multiplied by the standard deviation of the tail
             std_head = np.std(field_head[x_ind, y_ind])
             std_tail = np.std(field_tail[x_ind, y_ind])
-            gamma_standardized.append(gamma / (std_head * std_tail))
+            self.gamma_standardized.append(gamma / (std_head * std_tail))
 
-        return np.array(gamma_standardized)
+        self.gamma_standardized = np.array(self.gamma_standardized)
 
+    def generalized_exp_bessel(self, lag_vec, sill, ls, exp_pow, ls_hole_eff, nugget):
 
+        func = (sill - nugget)*(1 - np.exp(-(lag_vec/ls)**exp_pow)*special.j0(ls_hole_eff*lag_vec)) + nugget
+
+        return func
+
+    def get_widgets(self):
+
+        self._model_drop = widgets.Dropdown(options=['Exponential', 'Generalized exponential-Bessel'],
+                                            value='Exponential',
+                                            description='Variogram Model',
+                                            disabled=False, style={'description_width': 'initial'})
+
+        self._sill_box = widgets.FloatText(value=1.0,
+                                           description='Sill', disabled=False)
+
+        self._len_scale_box = widgets.FloatText(value=1.0,
+                                                description='Length Scale',
+                                                disabled=False,
+                                                style={'description_width': 'initial'})
+
+        self._exp_pow_box = widgets.FloatText(value=1.0,
+                                              description='Exponential power',
+                                              disabled=False,
+                                              style={'description_width': 'initial'})
+
+        self._ls_hole_eff_box = widgets.FloatText(value=1.0,
+                                                  description='Length scale hole effect',
+                                                  disabled=False,
+                                                  style={'description_width': 'initial'})
+
+        self._nugget_box = widgets.FloatText(value=0.0,
+                                             description='Nugget', disabled=False)
+
+        self._lsq_toggle = widgets.ToggleButton(value=False,
+                                                description='Run Least-squares fit',
+                                                disabled=False, button_style='info',
+                                                tooltip='Description',
+                                                icon='line-chart')
+
+    def plot_data(self,
+                  sill=1.0,
+                  len_scale=1.0,
+                  model='Generalized exponential-Bessel',
+                  lsq_fit=False):
+
+        plt.close()
+        fig = plt.figure(1, figsize=(8, 4), dpi=100)
+        fig.canvas.manager.set_window_title('')
+        plt.close(1)
+        ax = fig.add_subplot()
+        ax.plot(self._center_bins, self.gamma_standardized, 'bo')
+
+        # if model == 'Generalized exponential-Bessel':
+        #     ax.plot()
+        # else:
+        #     print("Not implemented!")
+
+        if lsq_fit:
+            ax.plot(0.002, 0.5, 'r.')
+
+        fig.tight_layout()
+        fig.show()
+
+    def view_semi_variogram(self):
+
+        if self._center_bins is None:
+            print("You must calculate the semi-variogram first!")
+            sys.exit()
+
+        self.get_widgets()
+
+        self._model_drop = None
+        self._sill_box = None
+        self._len_scale_box = None
+        self._exp_pow_box = None
+        self._ls_hole_eff_box = None
+        self._nugget_box = None
+        self._lsq_toggle = None
+
+        inter_out_exp = widgets.interactive_output(self.plot_data,
+                                                   {'sill': self._sill_box,
+                                                    'len_scale': self._len_scale_box,
+                                                    'model': self._model_drop,
+                                                    'lsq_fit': self._lsq_toggle})
+
+        grid = widgets.GridspecLayout(4, 2, width='80%', height='auto')
+
+        grid[0, 0] = self._sill_box
+        grid[1, 0] = self._len_scale_box
+        grid[2, 0] = self._model_drop
+        grid[3, 0] = self._lsq_toggle
+        grid[:, 1:] = inter_out_exp
+
+        return grid
 
 
