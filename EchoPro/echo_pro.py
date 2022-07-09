3d5567c71e109e12867819c412b901c61ebb186e
b-reyes <reyesb123@gmail.com>
2022-07-09 15:29:42 -0700

    slim down configuration files to only necessary parameters

--- EchoPro/echo_pro.py
+++ EchoPro/echo_pro.py
@@ -13,10 +13,11 @@ import pandas as pd
 
 class EchoPro:
     """
-    EchoPro base class that importing and prepares parameters for
+    EchoPro base class that imports and prepares parameters for
     later processes. Additionally, it includes functions for
-    accessing the processing, visualization, and generating reports
-    Classes.
+    accessing the classes associated with the biomass density
+    calculation, CV analysis, semi-variogram algorithm, and
+    Kriging.
 
     Parameters
     ----------
@@ -34,9 +35,6 @@ class EchoPro:
         1 = Acoustic Survey Trawl Survey
         2 = Bottom Trawl Survey
         3 = Observer Data
-    age_data_status : int
-        1 = actual age data
-        2 = from age_vs_len
     exclude_age1 : bool
         States whether age 1 hake should be included in analysis.
     stratification_index : int
@@ -52,8 +50,6 @@ class EchoPro:
                  init_file_path: str,
                  survey_year_file_path: str,
                  source: int = 3,
-                 bio_data_type: int = 1,
-                 age_data_status: int = 1,
                  exclude_age1: bool = True,
                  stratification_index: int = 1):
 
@@ -62,10 +58,13 @@ class EchoPro:
         self._check_init_file()
         self._check_survey_year_file()
 
-        init_params = self._read_initialization_config(init_file_path)
-        init_params = self._set_params_from_init(source, bio_data_type, init_params, age_data_status)
+        # read initialization configuration file
+        init_params = self._read_config(init_file_path)
 
-        survey_year_params = self._read_survey_year_config(survey_year_file_path)
+        init_params = self._set_params_from_init(source, init_params)
+
+        # read survey year configuration file
+        survey_year_params = self._read_config(survey_year_file_path)
 
         self.params = self._collect_parameters(init_params, survey_year_params)
 
@@ -89,11 +88,12 @@ class EchoPro:
 
         self.final_biomass_table = None
 
-        self._load_files(stratification_index)
+        self._load_files()
 
         self._compute_biomass_density()
 
     def _check_init_file(self):
+        """"""
         # TODO: create this function that checks the contents of the initialization config file
         # TODO: it should make sure that certain variables are defined too
         print("A check of the initialization file needs to be done!")
@@ -104,27 +104,23 @@ class EchoPro:
         print("A check of the survey year file needs to be done!")
 
     @staticmethod
-    def _read_initialization_config(init_file_path):
+    def _read_config(file_path):
+        """
+        Reads configuration files and returns a dictionary
+        with the parameters specified in the file.
+        """
 
-        with open(init_file_path) as f:
+        with open(file_path) as f:
+            params = yaml.load(f, Loader=yaml.SafeLoader)
 
-            init_params = yaml.load(f, Loader=yaml.SafeLoader)
-
-        return init_params
-
-    @staticmethod
-    def _read_survey_year_config(survey_year_file_path):
-        # TODO: This may need to be replaced in the future with a python file that way we can
-        # TODO: mirror proc_parameters_XXXX.m more closely (specifically the switch case statement)
-
-        with open(survey_year_file_path) as f:
-
-            survey_params = yaml.load(f, Loader=yaml.SafeLoader)
-
-        return survey_params
+        return params
 
     @staticmethod
-    def _set_params_from_init(source: int, bio_data_type: int, init_params: dict, age_data_status: int):
+    def _set_params_from_init(source: int, init_params: dict):
+        """
+        Constructs and assigns important variables using
+        parameters from the initialization configuration file.
+        """
 
         # setting bio_hake_lin_bin variable to a numpy array
         init_params["bio_hake_len_bin"] = np.linspace(init_params["bio_hake_len_bin"][0],
@@ -138,47 +134,15 @@ class EchoPro:
                                                       num=init_params["bio_hake_age_bin"][2],
                                                       dtype=np.int64)
 
-        # making acoust_freq0 into a numpy array
-        init_params["acoust_freq0"] = np.array(init_params["acoust_freq0"], dtype=np.float64)
-
-        # turning beamwidth input into radians
-        init_params["acoust_bw"] = np.array(init_params["acoust_bw"], dtype=np.float64)*np.pi/180.0
-
-        # finding the acoustic freqency index, default = 1 --> 38 kHz
-        # TODO: make sure zero indexing doesn't mess up downsteam items
-        init_params.update({'acoust_freq_ind': np.intersect1d(np.floor(init_params["acoust_freq"]/1000.0),
-                                                              init_params["acoust_freq0"],
-                                                              return_indices=True)[2]})
-
-        # Setting acoust_sig_b_coef to a float from string input
-        init_params["acoust_sig_b_coef"] = eval(init_params["acoust_sig_b_coef"])
-
-        # set variables based on the source used
-        if source <= 3:
-            init_params["platform_name"] = 'FSV'
-        else:  # TODO look into this and see if this else statement is necessary
-            init_params["platform_name"] = 'SD'
-            if bio_data_type != 3:
-                bio_data_type = 3
-                print("Changing bio_data_type to 3 based on platform name.")
-
-        init_params["opr_indx"] = 3  # TODO: look into this variable, might only be necessary for Matlab GUI
-
-        # setting the species code ID based on bio data type
-        if bio_data_type == 1:
-            init_params["species_code_ID"] = 22500  # target species_code for acoustic survey
-        elif bio_data_type == 2:
-            init_params["species_code_ID"] = 22500  # target species_code for bottom trawl survey
-        elif bio_data_type == 3:
-            init_params["species_code_ID"] = 206  # target species_code for industry data(observer data)
-
         init_params["source"] = source
-        init_params["bio_data_type"] = bio_data_type
-        init_params["age_data_status"] = age_data_status
 
         return init_params
 
     def _collect_parameters(self, init_params, survey_params):
+        """
+        Collects all parameters defined in the initialization
+        and survey year configuration files  into one variable.
+        """
 
         # check to make sure no survey year and initialization parameters are the same
         param_intersect = set(init_params.keys()).intersection(set(survey_params.keys()))
@@ -199,12 +163,6 @@ class EchoPro:
     def _load_nasc_data(self):
         """
         Load VL interval-based NASC table.
-
-        Parameters
-        ----------
-        exclude_age1 : bool
-            States whether age 1 hake should be included in analysis.
-
         Returns
         -------
         Pandas Dataframe of NASC table.
@@ -241,24 +199,10 @@ class EchoPro:
 
         return df
 
-    def _load_files(self, stratification_index):
+    def _load_files(self):
         """
-        Load the biological, NASC table, stratification file,
-        Constructs final trawl and catch tables
-
-        Parameters
-        ----------
-        stratification_index : int
-            Index for the chosen stratification
-            0 = INPFC strata
-            1 = KS (trawl)-based
-            2-6 = geographically based but close to trawl-based stratification
-            7 = mix-proportion, rather than 85% & 20% hake/hake-mix rules
-            10 = one stratum for the whole survey
-
-        Returns
-        -------
-
+        Loads the biological, NASC, and stratification
+        files then assigns them as variables of the class.
         """
 
         # load specimen and length data using EchoPro variables
