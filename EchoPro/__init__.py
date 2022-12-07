a7d2b7fcf8b180e9520d59e0dd359a9a66698163
b-reyes <reyesb123@gmail.com>
2022-12-07 10:25:28 -0800

    remove create preliminary files sub-package and its references

--- EchoPro/__init__.py
+++ EchoPro/__init__.py
@@ -1,9 +1,8 @@
 from _echopro_version import __version__
 
 from .survey import Survey
-from .create_preliminary_files import CreateFiles
 from .generate_reports import GenerateReports
 from .visualization import Visualize
 
-__all__ = ["Survey", "CreateFiles", "GenerateReports", "Visualize"]
+__all__ = ["Survey", "GenerateReports", "Visualize"]
 
