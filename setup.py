6fd54f7ae098144ee238e3751152b63049863e3c
Emilio Mayorga <emiliomayorga@gmail.com>
2023-01-18 19:51:26 -0800

    Remove leftover comments from setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-# from __future__ import absolute_import, division, print_function
-
 from setuptools import setup, find_packages
 
 # Dynamically read dependencies from requirements file
@@ -7,4 +5,4 @@ with open("requirements.txt") as f:
     requirements = f.readlines()
 
 if __name__ == "__main__":
-    setup(install_requires=requirements, packages=find_packages()) # ["EchoPro/."])
+    setup(install_requires=requirements, packages=find_packages())
