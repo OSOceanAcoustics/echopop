d59f2e3964bec5009a2569d33537c503449e563d
b-reyes <reyesb123@gmail.com>
2023-01-20 09:59:46 -0800

    rename biomass_density.py to transect_results.py

--- EchoPro/computation/__init__.py
+++ EchoPro/computation/__init__.py
@@ -1,11 +1,11 @@
 """
 This sub-package contains all routines that perform computation.
 """
-from .biomass_density import ComputeTransectVariables
 from .bootstrapping import Bootstrapping
 from .cv import run_jolly_hampton
 from .kriging import Kriging, krig_param_type, krig_type_dict
 from .semivariogram import SemiVariogram, vario_param_type, vario_type_dict
+from .transect_results import ComputeTransectVariables
 
 __all__ = [
     "ComputeTransectVariables",
