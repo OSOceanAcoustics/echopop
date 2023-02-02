9d97588daaa1f6f2965fbd85464d7ccc55838d65
b-reyes <53541061+b-reyes@users.noreply.github.com>
2023-02-02 11:42:51 -0800

    correct documentation in matlab_output_base_path
    Co-authored-by: Emilio Mayorga <emiliomayorga@gmail.com>

--- EchoPro/tests/conftest.py
+++ EchoPro/tests/conftest.py
@@ -25,6 +25,6 @@ def matlab_output_base_path() -> pathlib.Path:
     Returns
     -------
     pathlib.Path
-        The base directory path for the configuration files
+        The base directory path for the Matlab output files
     """
     return pathlib.Path("/Users/brandonreyes/UW_work/EchoPro_work/UW_EchoProMatlab_Repackaged/outputs/EchoPro_matlab_output_brandon_age_22_end_bin")
