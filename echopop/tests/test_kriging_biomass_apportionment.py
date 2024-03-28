2ea574e6b2c38fc0d9291150c522a0f5f0f4a3e5
Brandyn Lucca <brandyn.lucca@gmail.com>
2024-03-28 13:51:39 -0700

    `test_compute_index_aged_weight_proportions` edits
    - This includes:
    ** Reformatted tests for evaluating dataframe dimensions
    ** Dimensions of test dataframe were heavily reduced
    ** New hard-coded test dataframe w/ equality testing
    ** New datatype (`dtype`) test

--- echopop/tests/test_kriging_biomass_apportionment.py
+++ echopop/tests/test_kriging_biomass_apportionment.py
@@ -61,99 +61,86 @@ def test_sum_strata_weight( mock_survey ):
                             5.263158 , 8.631579 , 12.000000 , 15.368421 , 18.736842  ] )
     assert np.allclose( check_values , object_weight_strata_aged_unaged.stratum_weight )
 
-def test_compute_summed_aged_proportions( mock_survey ):
+def test_compute_index_aged_weight_proportions( mock_survey ):
     #### Pull in mock Survey object
     objS = mock_survey
 
     ### Re-parameterize `specimen_df` with dummy data 
     objS.biology[ 'specimen_df' ] = pd.DataFrame(
         {
-            'stratum_num': np.repeat( [ 0 , 1 , 2 , 4 , 5 ] , 4 ) ,
-            'sex': np.tile( [ 'male' , 'female' ] , 10 ) ,
-            'haul_num': np.repeat( [ 1 , 2 , 3 , 4 , 5 ] , 4 ) ,
-            'species_id': np.repeat( [ 19350 ] , 20 ) ,
-            'length': np.linspace( 10 , 20 , 20 ) ,
-            'weight': np.linspace( 1 , 5 , 20 ) ,
-            'age': [ 1 , 1 , 1 , 2 , 2 , 2 , 2 , 3 , 3 , 3 ,
-                     4 , 4 , 5 , 5 , 5 , 6 , 6 , 6 , 7 , 8 ]     
+            'stratum_num': np.repeat( [ 0 , 1 ] , 4 ) ,
+            'sex': np.tile( [ 'male' , 'female' ] , 4 ) ,
+            'haul_num': np.tile( [ 1 , 2 ] , 4 ) ,
+            'species_id': np.repeat( [ 19350 ] , 8 ) ,
+            'length': [ 10 , 20 , 19 , 11 , 12 , 18 , 17 , 13 ] ,
+            'weight': np.linspace( 1 , 5 , 8 ) ,
+            'age': [ 1 , 2 , 1 , 2 , 1 , 2 , 1 , 2 ]    
         }
     )
 
     ### Length interval
-    objS.biology[ 'distributions' ][ 'length' ][ 'length_interval_arr' ] = np.linspace( 9 , 21 , 5 )
+    objS.biology[ 'distributions' ][ 'length' ][ 'length_interval_arr' ] = np.linspace( 9 , 21 , 3 )
     
     ### Age interval
-    objS.biology[ 'distributions' ][ 'age' ][ 'age_interval_arr' ] = (
-        np.array( [ 0.5 , 1.5 , 2.5 , 3.5 , 4.5 , 5.5 , 6.5 , 7.5 , 8.5 ] )
-    ) 
+    objS.biology[ 'distributions' ][ 'age' ][ 'age_interval_arr' ] = np.array( [ 0.5 , 1.5 , 2.5 ] )
 
     ### Evaluate object for later comparison 
     obj_props_wgt_len_age_sex = compute_index_aged_weight_proportions( objS.biology[ 'specimen_df' ] ,
                                                                        objS.biology[ 'distributions' ][ 'length' ][ 'length_interval_arr' ] ,
                                                                        objS.biology[ 'distributions' ][ 'age' ][ 'age_interval_arr' ] )
+    ###--------------------------------
+    ### Expected outcomes
+    ###--------------------------------
+    # ---- Expected dimensions of `obj_props_wgt_len_age_sex`   
+    expected_dimensions = tuple( [ 16 , 11 ] )
     
+    # ---- Expected dataframe output
+    expected_output = pd.DataFrame( {
+        'stratum_num': np.repeat( [ 0 , 1 ] , 8 ).astype( np.int64 ) ,
+        'species_id': np.repeat( 19350 , 16 ).astype( np.int64 ) ,
+        'sex': np.tile( [ 'female' , 'male' ] , 8 ).astype( object ) ,
+        'length_bin': pd.IntervalIndex.from_arrays( np.tile( [9.0 , 15.0 ] , 8 ) , 
+                                                    np.tile( [ 15.0 , 21.0 ] , 8 ) , 
+                                                    closed = 'right' ) ,
+        'age_bin': pd.IntervalIndex.from_arrays( np.tile( [ 0.5, 1.5 ] , 8 ) , 
+                                                 np.tile( [ 1.5 , 2.5 ] , 8 ) , 
+                                                 closed = 'right' ) ,
+        'weight_all': [ 0.0, 2.714286, 0.0, 1.571429, 1.0, 0.0, 2.142857, 0.0, 0.0, 
+                        5.0, 0.0, 3.857143, 3.285714, 0.0, 4.428571, 0.0 ] ,
+        'weight_adult': [ 0.0, 2.714286, 0.0, 1.571429, 0.0, 0.0, 0.0, 0.0, 0.0, 5.0, 
+                          0.0, 3.857143, 0.0, 0.0, 0.0, 0.0 ] ,
+        'total_weight_sex_all': [ 4.285714, 4.285714, 4.285714, 4.285714, 3.142857, 
+                                  3.142857, 3.142857, 3.142857, 8.857143, 8.857143, 
+                                  8.857143, 8.857143, 7.714286, 7.714286, 7.714286, 
+                                  7.714286 ] ,
+        'total_weight_sex_adult': [ 4.285714, 4.285714, 4.285714, 4.285714, 0.0, 0.0, 
+                                   0.0, 0.0, 8.857143, 8.857143, 8.857143, 8.857143, 
+                                   0.0, 0.0, 0.0, 0.0 ] ,
+        'proportion_weight_sex_all': [ 0.0, 0.633333, 0.0, 0.366667, 0.318182, 0.0, 
+                                      0.681818, 0.0, 0.0, 0.564516, 0.0, 0.435484, 
+                                      0.425926, 0.0, 0.574074, 0.0 ] ,
+        'proportion_weight_sex_adult': [ 0.0, 0.633333, 0.0, 0.366667, 0.0, 0.0, 0.0, 
+                                        0.0, 0.0, 0.564516, 0.0, 0.435484, 0.0, 0.0, 
+                                        0.0, 0.0 ]
+    } )
+    expected_output[ 'length_bin' ] = pd.IntervalIndex( expected_output[ 'length_bin' ] )
+    expected_output[ 'length_bin' ] = pd.Categorical( expected_output[ 'length_bin' ] , 
+                                                      categories =  expected_output[ 'length_bin' ].unique( ) , 
+                                                      ordered = True )
+    expected_output[ 'age_bin' ] = pd.IntervalIndex( expected_output[ 'age_bin' ] )
+    expected_output[ 'age_bin' ] = pd.Categorical( expected_output[ 'age_bin' ] , 
+                                                 categories = expected_output[ 'age_bin' ].unique( ) , 
+                                                 ordered=True)
+
     #----------------------------------
     ### Run tests: `compute_index_aged_weight_proportions`
     #----------------------------------
     ### Process the specimen data 
-    specimen_binned = (
-         objS.biology[ 'specimen_df' ]
-        # ---- Drop unaged fish 
-        # !!! TODO: pending what FEAT says, weights associated with 
-        # missing ages should be added into the 'unaged' category. 
-        # This would further mean apportioning these into `weight_strata_aged_uanged`
-        .dropna( how = 'any' )
-        # ---- Remove unsexed fish
-        .loc[ lambda df: df.sex != 'unsexed' ]
-        # ---- Bin length
-        .bin_variable( objS.biology[ 'distributions' ][ 'length' ][ 'length_interval_arr' ] , 'length' )
-        # ---- Age bin
-        .bin_variable( objS.biology[ 'distributions' ][ 'age' ][ 'age_interval_arr' ] , 'age' )
-    )
-
-    ### Sum weights within each length and age bin for each sex within each stratum
-    specimen_binned_weight = (
-        specimen_binned
-        # ---- Group weight summations across distributions of length and age 
-        # ---- for each sex within each stratum
-        .groupby( [ 'stratum_num' , 'sex' , 'length_bin' , 'age_bin' ] )
-        # ---- Sum the weights 
-        .apply( lambda df: pd.Series( { 'weight_all': df.weight.sum( ) ,
-                                        'weight_adult': df.loc[ df.age > 1 ].weight.sum( ) } ) ) 
-        # ---- Fill empty/non-existent values with 0's
-        .fillna( 0 )
-        .reset_index( )
-    )
-
-    ### Calculate the relative weight proportions of each length-age bin for each sex within each stratum
-    proportions_weight_length_age_sex = (
-        specimen_binned_weight
-        # ---- Calculate total sex-specific weights for each stratum
-        .assign( total_weight_sex_all = lambda df: df.groupby( [ 'stratum_num' , 'sex' ] )[ 'weight_all' ].transform( sum ) ,
-                 total_weight_sex_adult = lambda df: df.groupby( [ 'stratum_num' , 'sex' ] )[ 'weight_adult' ].transform( sum ) )
-        # ---- Calculate the weight proportions within each sex: from Matlab --> Len_age_key_wgt_*n
-        .assign( proportion_weight_sex_all = lambda df: df.weight_all / df.total_weight_sex_all ,
-                 proportion_weight_sex_adult = lambda df: df.weight_adult / df.total_weight_sex_adult )
-    )
-
-    ### Fill empty/non-existent values with 0's
-    proportions_weight_length_age_sex[ 'proportion_weight_sex_all' ] = (
-        proportions_weight_length_age_sex[ 'proportion_weight_sex_all' ].fillna( 0 )
-    )
-
-    proportions_weight_length_age_sex[ 'proportion_weight_sex_adult' ] = (
-        proportions_weight_length_age_sex[ 'proportion_weight_sex_adult' ].fillna( 0 )
-    )
-
     ### Check shape 
-    test_dimensions = (
-        len( np.unique( specimen_binned.age_bin ) ) *
-        len( np.unique( specimen_binned.length_bin ) ) *
-        len( np.unique( specimen_binned.sex ) ) *
-        len( np.unique( specimen_binned.stratum_num ) )
-    )
+    assert obj_props_wgt_len_age_sex.shape == expected_dimensions
 
-    assert obj_props_wgt_len_age_sex.shape == tuple( [ test_dimensions , 10 ] )
-
-    ### Check values
-    assert obj_props_wgt_len_age_sex.equals( proportions_weight_length_age_sex )
+    ### Check datatypes
+    assert np.all( obj_props_wgt_len_age_sex.dtypes == expected_output.dtypes )
+    
+    ### Check data value equality
+    assert np.all( obj_props_wgt_len_age_sex.dtypes == expected_output.dtypes )
