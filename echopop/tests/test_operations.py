c2a1d964cb824c299a57201530df4ecddbca651a
Brandyn Lucca <brandyn.lucca@gmail.com>
2024-04-05 17:40:38 -0700

    `test_bin_stats`

--- echopop/tests/test_operations.py
+++ echopop/tests/test_operations.py
@@ -45,3 +45,105 @@ def test_bin_variable( ):
     ### Check output
     assert eval_dataframe_monkey.equals( expected_output )
     assert eval_dataframe_function.equals( expected_output )
+
+def test_bin_stats( ):
+
+    ### Mock dataframe
+    test_dataframe = pd.DataFrame( 
+        {
+            'animal': [ 'pretty pink pony' , 'big blue bass' , 'silly silver silkworm' ,
+                        'gnarly green grouse' , 'roudy red rabbit' , 'magenta mad manatee' ] ,
+            'length': [ 2.0 , 4.0 , 8.0 , 3.0 , 6.0 , 7.0 ] ,
+            'weight': [ 100.0 , 200.0 , 300.0 , 300.0 , 200.0 , 100.0 ] ,
+            'location': [ 'timbuktu' , 'timbuktu' , 'timbuktu' ,
+                          'lost city of z' , 'lost city of z' , 'lost city of z' ] ,
+        } ,
+    )
+
+    ### Mock bin_values
+    test_bin_values = np.array( [ 1.0 , 3.0 , 5.0 , 7.0 , 9.0 ] )
+
+    ### Evaluate for later comparison
+    # ++++ No contrast | length + weight
+    # ---- Monkey patch method (TEMPORARY)
+    eval_dataframe_monkey_lwnc = test_dataframe.bin_stats( 'length' , test_bin_values )
+    # ---- Normal function
+    eval_dataframe_function_lwnc = bin_stats( test_dataframe , 'length' , test_bin_values )
+    # ++++ No contrast | length
+    eval_dataframe_monkey_lnc = test_dataframe.bin_stats( 'length' , test_bin_values , variables = 'length' )
+    # ---- Normal function
+    eval_dataframe_function_lnc = bin_stats( test_dataframe , 'length' , test_bin_values , variables = 'length' )
+    # ++++ No contrast | length ~ function: just mean
+    eval_dataframe_monkey_lncm = test_dataframe.bin_stats( 'length' , test_bin_values , variables = 'length' , functions = [ 'mean' ] )
+    # ---- Normal function
+    eval_dataframe_function_lncm = bin_stats( test_dataframe , 'length' , test_bin_values , variables = 'length' , functions = [ 'mean' ] )
+    # ++++ No contrast | length ~ function: just mean
+    eval_dataframe_monkey_lwc = test_dataframe.bin_stats( 'length' , test_bin_values , contrasts = [ 'location' ] , variables = 'length' )
+    # ---- Normal function
+    eval_dataframe_function_lwc = bin_stats( test_dataframe , 'length' , test_bin_values , contrasts = [ 'location' ] , variables = 'length' )
+
+    ###--------------------------------
+    ### Expected outcomes
+    ###--------------------------------
+    # ---- Expected dimensions
+    expected_dimensions_lwnc = tuple( [ 4 , 5 ] )
+    expected_dimensions_lnc = tuple( [ 4 , 3 ] )
+    expected_dimensions_lncm = tuple( [ 4 , 2 ] )
+    expected_dimensions_lwc = tuple( [ 8 , 4 ] )
+    # ---- Expected output
+    expected_output_lwnc = pd.DataFrame(
+        {   
+            'length_bin': pd.cut( [ 2.0 , 4.0 , 6.0 , 8.0 ] ,
+                                  np.array( [ 1.0 , 3.0 , 5.0 , 7.0 , 9.0 ] ) ) ,
+            'mean_length': [ 2.5 , 4.0 , 6.5 , 8.0 ] ,
+            'n_length': [ 2 , 1 , 2 , 1 ] ,
+            'mean_weight': [ 200.0 , 200.0 , 150.0 , 300.0 ] ,
+            'n_weight': [ 2 , 1 , 2 , 1 ] ,
+        } ,
+    )
+    expected_output_lnc = pd.DataFrame(
+        {   
+            'length_bin': pd.cut( [ 2.0 , 4.0 , 6.0 , 8.0 ] ,
+                                  np.array( [ 1.0 , 3.0 , 5.0 , 7.0 , 9.0 ] ) ) ,
+            'mean_length': [ 2.5 , 4.0 , 6.5 , 8.0 ] ,
+            'n_length': [ 2 , 1 , 2 , 1 ] ,
+        } ,
+    )
+    expected_output_lncm = pd.DataFrame(
+        {   
+            'length_bin': pd.cut( [ 2.0 , 4.0 , 6.0 , 8.0 ] ,
+                                  np.array( [ 1.0 , 3.0 , 5.0 , 7.0 , 9.0 ] ) ) ,
+            'mean_length': [ 2.5 , 4.0 , 6.5 , 8.0 ]
+        } ,
+    )
+    expected_output_lwc = pd.DataFrame(
+        {   
+            'length_bin': pd.cut( np.repeat( [ 2.0 , 4.0 , 6.0 , 8.0 ] , 2 ) ,
+                                  np.array( [ 1.0 , 3.0 , 5.0 , 7.0 , 9.0 ] ) ) ,
+            'location': np.tile( [ 'lost city of z' , 'timbuktu' ] , 4 ) ,
+            'mean_length': [ 3.0 , 2.0 , 0.0 , 4.0 , 6.5 , 0.0 , 0.0 , 8.0 ] ,
+            'n_length': [ 1 , 1 , 0 , 1 , 2 , 0 , 0 , 1 ] ,
+        } ,
+    )
+
+    #----------------------------------
+    ### Run tests: `bin_stats`
+    #----------------------------------
+    ### Check shape
+    assert eval_dataframe_monkey_lwnc.shape == expected_dimensions_lwnc
+    assert eval_dataframe_function_lwnc.shape == expected_dimensions_lwnc
+    assert eval_dataframe_monkey_lnc.shape == expected_dimensions_lnc
+    assert eval_dataframe_function_lnc.shape == expected_dimensions_lnc
+    assert eval_dataframe_monkey_lncm.shape == expected_dimensions_lncm
+    assert eval_dataframe_function_lncm.shape == expected_dimensions_lncm
+    assert eval_dataframe_monkey_lwc.shape == expected_dimensions_lwc
+    assert eval_dataframe_function_lwc.shape == expected_dimensions_lwc
+    ### Check output
+    assert eval_dataframe_monkey_lwnc.equals( expected_output_lwnc )
+    assert eval_dataframe_function_lwnc.equals( expected_output_lwnc )
+    assert eval_dataframe_monkey_lnc.equals( expected_output_lnc )
+    assert eval_dataframe_function_lnc.equals( expected_output_lnc )
+    assert eval_dataframe_monkey_lncm.equals( expected_output_lncm )
+    assert eval_dataframe_function_lncm.equals( expected_output_lncm )
+    assert eval_dataframe_monkey_lwc.equals( expected_output_lwc )
+    assert eval_dataframe_function_lwc.equals( expected_output_lwc )
