6a2d70dc4c0faf8690f944a036aa91d7a9cd2c36
Brandyn Lucca <brandyn.lucca@gmail.com>
2024-05-28 07:46:07 -0700

    Leftover code from `__init__.py` removed

--- echopop/__init__.py
+++ echopop/__init__.py
@@ -1,11 +1,6 @@
-import Path
 from .survey import Survey
 from .utils import operations
 
 __all__ = ["Survey", "operations"]
 
 from _echopop_version import version as __version__  # noqa
-
-# Define root path
-HERE = Path(__file__).parent.absolute()
-TEST_DATA_ROOT = HERE.parent / "test_data"
