c5a5a7285cb49c853f68a103e4fa09e1ac8a38f9
Wu-Jung Lee <leewujung@gmail.com>
2024-11-02 15:34:39 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-version = "0.4.1"
+from setuptools import setup
 
 # Dynamically read dependencies from requirements file
 with open("requirements.txt") as f:
     requirements = f.readlines()
 
 if __name__ == "__main__":
-    setup(version=version, install_requires=requirements, packages=find_packages())
+    setup(install_requires=requirements)
