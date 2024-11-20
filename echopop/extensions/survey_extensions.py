2640dca2c6c23b0993a9f466007adba60a902483
Brandyn Lucca <brandyn.lucca@gmail.com>
2024-11-20 15:32:32 -0800

    Hotfix to patcher

--- echopop/extensions/survey_extensions.py
+++ echopop/extensions/survey_extensions.py
@@ -1,6 +1,7 @@
 from ..survey import Survey
 from .feat_report import FEATReports
 import functools
+from typing import List
 
 ####################################################################################################
 # PATCH METHODS
@@ -14,6 +15,11 @@ def _generate_reports(self: Survey, **kwargs) -> None:
     # Return the result for `FEATReports` class methods
     return reports_instance.generate()
 
+def _report_options(self) -> List[str]:
+
+    # Get report options
+    return FEATReports.report_options()
+
 ####################################################################################################
 # PATCHERS
 # --------------------------------------------------------------------------------------------------
@@ -23,8 +29,14 @@ def patch_generate_reports():
     """
     
     # Copy all hidden attributes
-    functools.update_wrapper(_generate_reports, FEATReports.generate)
+    # ---- `generate()`
+    functools.update_wrapper(_generate_reports, FEATReports)
+    # ---- `report_options()`
+    functools.update_wrapper(_report_options, FEATReports.report_options)
     
     # Assign method
+    # ---- `generate()`
     Survey.generate_reports = _generate_reports
+    # ---- `report_options()`
+    Survey.report_options = _report_options
 
