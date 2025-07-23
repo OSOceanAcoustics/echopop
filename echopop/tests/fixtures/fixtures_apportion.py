25eaa6590250fd29463d569318a938133e53491c
Brandyn Lucca <brandyn.lucca@gmail.com>
2025-07-22 17:53:16 -0700

    Pre-commit fixes

--- echopop/tests/fixtures/fixtures_apportion.py
+++ echopop/tests/fixtures/fixtures_apportion.py
@@ -1,109 +1,186 @@
+import numpy as np
+import pandas as pd
 import pytest
 
-import pandas as pd
-import numpy as np
-
 from echopop.nwfsc_feat import apportion
+
 
 @pytest.fixture
 def apportion_mesh():
     """Mesh dataset for apportionment tests"""
 
-    return pd.DataFrame({
-        "mesh_stratum": [1, 2, 3],
-        "biomass_density": [1e6, 2e6, 3e6],
-        "area": [10, 20, 30]
-    })
+    return pd.DataFrame(
+        {"mesh_stratum": [1, 2, 3], "biomass_density": [1e6, 2e6, 3e6], "area": [10, 20, 30]}
+    )
+
 
 @pytest.fixture
 def apportion_number_proportions():
     """Number proportions for apportionment tests"""
 
-    subgroup1 = pd.DataFrame({
-        "bio_stratum": np.tile([1, 2, 3], 6),
-        "contrast": np.repeat(["A", "B"], 9),
-        "index_bin": np.concatenate([np.repeat([5, 10, 15], 3), 
-                                     np.repeat([5, 10, 15], 3)]),
-        "extra_bin": np.tile([1, 2], 9),
-        "proportion_overall": [0.10, 0.20, 0.10, 
-                               0.10, 0.10, 0.00, 
-                               0.00, 0.00, 0.10,  
-                               0.20, 0.00, 0.00, 
-                               0.00, 0.20, 0.10,  
-                               0.10, 0.30, 0.20]
-    })
-    subgroup2 = pd.DataFrame({
-        "bio_stratum": np.tile([1, 2, 3], 6),
-        "contrast": np.repeat(["A", "B"], 9),
-        "index_bin": np.concatenate([np.repeat([5, 10, 15], 3), 
-                                     np.repeat([5, 10, 15], 3)]),
-        "proportion_overall": [0.10, 0.20, 0.20, 
-                               0.00, 0.00, 0.20, 
-                               0.10, 0.00, 0.10,  
-                               0.30, 0.00, 0.00, 
-                               0.00, 0.00, 0.00,  
-                               0.00, 0.00, 0.00]
-    })
+    subgroup1 = pd.DataFrame(
+        {
+            "bio_stratum": np.tile([1, 2, 3], 6),
+            "contrast": np.repeat(["A", "B"], 9),
+            "index_bin": np.concatenate([np.repeat([5, 10, 15], 3), np.repeat([5, 10, 15], 3)]),
+            "extra_bin": np.tile([1, 2], 9),
+            "proportion_overall": [
+                0.10,
+                0.20,
+                0.10,
+                0.10,
+                0.10,
+                0.00,
+                0.00,
+                0.00,
+                0.10,
+                0.20,
+                0.00,
+                0.00,
+                0.00,
+                0.20,
+                0.10,
+                0.10,
+                0.30,
+                0.20,
+            ],
+        }
+    )
+    subgroup2 = pd.DataFrame(
+        {
+            "bio_stratum": np.tile([1, 2, 3], 6),
+            "contrast": np.repeat(["A", "B"], 9),
+            "index_bin": np.concatenate([np.repeat([5, 10, 15], 3), np.repeat([5, 10, 15], 3)]),
+            "proportion_overall": [
+                0.10,
+                0.20,
+                0.20,
+                0.00,
+                0.00,
+                0.20,
+                0.10,
+                0.00,
+                0.10,
+                0.30,
+                0.00,
+                0.00,
+                0.00,
+                0.00,
+                0.00,
+                0.00,
+                0.00,
+                0.00,
+            ],
+        }
+    )
 
     return {"subgroup1": subgroup1, "subgroup2": subgroup2}
+
 
 @pytest.fixture
 def apportion_weight_proportions():
     """Weight proportions for apportionment tests"""
 
-    subgroup1 = pd.DataFrame({
-        "bio_stratum": np.tile([1, 2, 3], 6),
-        "contrast": np.repeat(["A", "B"], 9),
-        "index_bin": np.concatenate([np.repeat([5, 10, 15], 3), 
-                                     np.repeat([5, 10, 15], 3)]),
-        "extra_bin": np.tile([1, 2], 9),
-        "proportions": [0.10, 0.20, 0.10, 
-                        0.10, 0.10, 0.00, 
-                        0.00, 0.00, 0.10,  
-                        0.20, 0.00, 0.00, 
-                        0.00, 0.20, 0.10,  
-                        0.10, 0.30, 0.20]
-    }).pivot_table(index=["contrast", "index_bin", "extra_bin"], 
-                   columns=["bio_stratum"], 
-                   values="proportions").fillna(0.)    
-    subgroup2 = pd.DataFrame({
-        "bio_stratum": np.tile([1, 2, 3], 6),
-        "contrast": np.repeat(["A", "B"], 9),
-        "index_bin": np.concatenate([np.repeat([5, 10, 15], 3), 
-                                     np.repeat([5, 10, 15], 3)]),
-        "proportions": [0.10, 0.20, 0.20, 
-                        0.00, 0.00, 0.20, 
-                        0.10, 0.00, 0.10,  
-                        0.30, 0.00, 0.00, 
-                        0.00, 0.00, 0.00,  
-                        0.00, 0.00, 0.00]
-    }).pivot_table(index=["contrast", "index_bin"], 
-                   columns=["bio_stratum"], 
-                   values="proportions")
+    subgroup1 = (
+        pd.DataFrame(
+            {
+                "bio_stratum": np.tile([1, 2, 3], 6),
+                "contrast": np.repeat(["A", "B"], 9),
+                "index_bin": np.concatenate([np.repeat([5, 10, 15], 3), np.repeat([5, 10, 15], 3)]),
+                "extra_bin": np.tile([1, 2], 9),
+                "proportions": [
+                    0.10,
+                    0.20,
+                    0.10,
+                    0.10,
+                    0.10,
+                    0.00,
+                    0.00,
+                    0.00,
+                    0.10,
+                    0.20,
+                    0.00,
+                    0.00,
+                    0.00,
+                    0.20,
+                    0.10,
+                    0.10,
+                    0.30,
+                    0.20,
+                ],
+            }
+        )
+        .pivot_table(
+            index=["contrast", "index_bin", "extra_bin"],
+            columns=["bio_stratum"],
+            values="proportions",
+        )
+        .fillna(0.0)
+    )
+    subgroup2 = pd.DataFrame(
+        {
+            "bio_stratum": np.tile([1, 2, 3], 6),
+            "contrast": np.repeat(["A", "B"], 9),
+            "index_bin": np.concatenate([np.repeat([5, 10, 15], 3), np.repeat([5, 10, 15], 3)]),
+            "proportions": [
+                0.10,
+                0.20,
+                0.20,
+                0.00,
+                0.00,
+                0.20,
+                0.10,
+                0.00,
+                0.10,
+                0.30,
+                0.00,
+                0.00,
+                0.00,
+                0.00,
+                0.00,
+                0.00,
+                0.00,
+                0.00,
+            ],
+        }
+    ).pivot_table(index=["contrast", "index_bin"], columns=["bio_stratum"], values="proportions")
 
     return {"subgroup1": subgroup1, "subgroup2": subgroup2}
+
 
 @pytest.fixture
 def apportion_stratum_weights():
     """Average weights per bin for each stratum for apportionment tests"""
 
-    return pd.DataFrame({
-        "bio_stratum": [1, 2, 3],
-        "global": [1.0, 2.0, 3.0],
-    }).set_index("bio_stratum")["global"]
+    return pd.DataFrame(
+        {
+            "bio_stratum": [1, 2, 3],
+            "global": [1.0, 2.0, 3.0],
+        }
+    ).set_index(
+        "bio_stratum"
+    )["global"]
+
 
 @pytest.fixture
 def apportion_stratum_sigma_bs():
     """Average sigma_bs for each stratum for apportionment tests"""
 
-    return pd.DataFrame({
-        "bio_stratum": [1, 2, 3],
-        "sigma_bs": [1e-3, 1e-4, 1e-5],
-    }).set_index(["bio_stratum"])
+    return pd.DataFrame(
+        {
+            "bio_stratum": [1, 2, 3],
+            "sigma_bs": [1e-3, 1e-4, 1e-5],
+        }
+    ).set_index(["bio_stratum"])
+
 
 @pytest.fixture
-def apportion_mesh_with_nasc(apportion_mesh, apportion_weight_proportions, 
-                             apportion_stratum_weights, apportion_stratum_sigma_bs):
+def apportion_mesh_with_nasc(
+    apportion_mesh,
+    apportion_weight_proportions,
+    apportion_stratum_weights,
+    apportion_stratum_sigma_bs,
+):
     """Pre-calculated mesh NASC for apportionment tests"""
 
     # Create copy of apportion mesh DataFrame
@@ -122,6 +199,7 @@ def apportion_mesh_with_nasc(apportion_mesh, apportion_weight_proportions,
     # Return
     return mesh_data_df
 
+
 @pytest.fixture
 def apportion_biomass_table(apportion_mesh_with_nasc, apportion_weight_proportions):
     """Pre-calculated apportioned biomass tables for apportionment tests"""
@@ -137,6 +215,7 @@ def apportion_biomass_table(apportion_mesh_with_nasc, apportion_weight_proportio
 
     return biomass_tables
 
+
 @pytest.fixture
 def apportion_biomass_table_with_standardized(apportion_biomass_table):
     """Pre-calculate apportioned biomass tables with standardized group for apportionment tests"""
@@ -150,10 +229,11 @@ def apportion_biomass_table_with_standardized(apportion_biomass_table):
         reference_table=apportion_biomass_table["subgroup1"],
         group_by=["contrast"],
         impute=True,
-        impute_variable=["extra_bin"],    
+        impute_variable=["extra_bin"],
     )
 
     return table_copy
+
 
 @pytest.fixture
 def apportion_combined_biomass_table(apportion_biomass_table_with_standardized):
--- echopop/tests/processing/test_apportion.py
+++ echopop/tests/processing/test_apportion.py
@@ -1,11 +1,16 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import pytest
 
 from echopop.nwfsc_feat import apportion
 
-def test_mesh_biomass_to_nasc(apportion_mesh, apportion_weight_proportions, 
-                              apportion_stratum_weights, apportion_stratum_sigma_bs):
+
+def test_mesh_biomass_to_nasc(
+    apportion_mesh,
+    apportion_weight_proportions,
+    apportion_stratum_weights,
+    apportion_stratum_sigma_bs,
+):
     """
     Test biomass to NASC conversion
     """
@@ -40,11 +45,19 @@ def test_mesh_biomass_to_nasc(apportion_mesh, apportion_weight_proportions,
     assert mesh_data_df.shape == (3, 10)
 
     # Check columns
-    assert (
-        sorted(mesh_data_df.columns) == sorted(
-            ["mesh_stratum", "biomass_density", "area", "biomass", "biomass_A",
-             "biomass_B", "abundance", "abundance_A", "abundance_B", "nasc"]
-        )
+    assert sorted(mesh_data_df.columns) == sorted(
+        [
+            "mesh_stratum",
+            "biomass_density",
+            "area",
+            "biomass",
+            "biomass_A",
+            "biomass_B",
+            "abundance",
+            "abundance_A",
+            "abundance_B",
+            "nasc",
+        ]
     )
 
     # Check values
@@ -56,9 +69,10 @@ def test_mesh_biomass_to_nasc(apportion_mesh, apportion_weight_proportions,
     assert np.allclose(mesh_data_df["abundance_B"], [1.0e7, 2.0e7, 3.0e7])
     assert np.allclose(np.round(mesh_data_df["nasc"], 4), [50265.4825, 12566.3706, 2638.9378])
 
-def test_distribute_kriged_estimates(apportion_mesh_with_nasc, 
-                                     apportion_weight_proportions,
-                                     apportion_number_proportions):
+
+def test_distribute_kriged_estimates(
+    apportion_mesh_with_nasc, apportion_weight_proportions, apportion_number_proportions
+):
     """
     Test distribution of kriged estimates
     """
@@ -105,6 +119,7 @@ def test_distribute_kriged_estimates(apportion_mesh_with_nasc,
     assert all(abundance_tables["subgroup1"].sum() == np.array([2.00e6, 8.00e6, 1.05e7]))
     assert all(abundance_tables["subgroup2"].sum() == np.array([2.00e6, 2.00e6, 1.05e7]))
 
+
 def test_standardize_kriged_estimates(apportion_biomass_table):
     """
     Test group-standardization of kriged estimates
@@ -115,7 +130,7 @@ def test_standardize_kriged_estimates(apportion_biomass_table):
         population_table=apportion_biomass_table["subgroup2"],
         reference_table=apportion_biomass_table["subgroup1"],
         group_by=["contrast"],
-        impute=False,    
+        impute=False,
     )
 
     # Check shape
@@ -125,32 +140,34 @@ def test_standardize_kriged_estimates(apportion_biomass_table):
     assert list(std_tbl_no_imp.index.names) == ["index_bin"]
     assert all(std_tbl_no_imp.index == pd.Index([5, 10, 15]))
     assert list(std_tbl_no_imp.columns.names) == ["extra_bin", "contrast"]
-    assert all(std_tbl_no_imp.columns == pd.MultiIndex.from_tuples(
-        [(1, "A"), (2, "A"), (1, "B"), (2, "B")]
-        )
+    assert all(
+        std_tbl_no_imp.columns
+        == pd.MultiIndex.from_tuples([(1, "A"), (2, "A"), (1, "B"), (2, "B")])
     )
 
     # Check values
     # --- Expected values
     expected_no_imp = pd.DataFrame(
-        {"extra_bin": [1, 2, 1, 2], 
-         "contrast": ["A", "A", "B", "B"], 
-         "value": [3.94e7, 1.56e7, 0.00, 3.00e6]}
+        {
+            "extra_bin": [1, 2, 1, 2],
+            "contrast": ["A", "A", "B", "B"],
+            "value": [3.94e7, 1.56e7, 0.00, 3.00e6],
+        }
     ).set_index(["extra_bin", "contrast"])["value"]
-    
+
     assert all(std_tbl_no_imp.sum() == expected_no_imp)
 
     # Standardize 'subgroup2' based on 'subgroup1' -- Imputation
     # ---- Mock a missing value
     apportion_biomass_table_imp = apportion_biomass_table.copy()
-    apportion_biomass_table_imp["subgroup1"].loc[[("A", 5, 1), ("B", 15, 2)]] = 0.
-    # ---- Run standardization    
+    apportion_biomass_table_imp["subgroup1"].loc[[("A", 5, 1), ("B", 15, 2)]] = 0.0
+    # ---- Run standardization
     std_tbl_imp = apportion.standardize_kriged_estimates(
         population_table=apportion_biomass_table_imp["subgroup2"],
         reference_table=apportion_biomass_table_imp["subgroup1"],
         group_by=["contrast"],
-        impute=True, 
-        impute_variable=["extra_bin"],   
+        impute=True,
+        impute_variable=["extra_bin"],
     )
 
     # Check inequality
@@ -163,20 +180,22 @@ def test_standardize_kriged_estimates(apportion_biomass_table):
     assert list(std_tbl_imp.index.names) == ["index_bin"]
     assert all(std_tbl_imp.index == pd.Index([5, 10, 15]))
     assert list(std_tbl_imp.columns.names) == ["extra_bin", "contrast"]
-    assert all(std_tbl_imp.columns == pd.MultiIndex.from_tuples(
-        [(1, "A"), (2, "A"), (1, "B"), (2, "B")]
-        )
+    assert all(
+        std_tbl_imp.columns == pd.MultiIndex.from_tuples([(1, "A"), (2, "A"), (1, "B"), (2, "B")])
     )
 
     # Check values
     # --- Expected values
     expected_imp = pd.DataFrame(
-        {"extra_bin": [1, 2, 1, 2], 
-         "contrast": ["A", "A", "B", "B"], 
-         "value": [2.44e7, 3.06e7, 0.00, 3.00e6]}
+        {
+            "extra_bin": [1, 2, 1, 2],
+            "contrast": ["A", "A", "B", "B"],
+            "value": [2.44e7, 3.06e7, 0.00, 3.00e6],
+        }
     ).set_index(["extra_bin", "contrast"])["value"]
-    
+
     assert all(std_tbl_imp.sum() == expected_imp)
+
 
 def test_combine_population_tables(apportion_biomass_table_with_standardized):
     """
@@ -202,9 +221,9 @@ def test_combine_population_tables(apportion_biomass_table_with_standardized):
     assert list(df_biomass_table.index.names) == ["index_bin"]
     assert all(df_biomass_table.index == pd.Index([5, 10, 15]))
     assert list(df_biomass_table.columns.names) == ["extra_bin", "contrast"]
-    assert all(df_biomass_table.columns == pd.MultiIndex.from_tuples(
-        [(1, "A"), (1, "B"), (2, "A"), (2, "B")]
-        )
+    assert all(
+        df_biomass_table.columns
+        == pd.MultiIndex.from_tuples([(1, "A"), (1, "B"), (2, "A"), (2, "B")])
     )
 
     # Check values
@@ -213,7 +232,8 @@ def test_combine_population_tables(apportion_biomass_table_with_standardized):
     DF_B = apportion_biomass_table_with_standardized["standardized_subgroup2"].copy()
     DF_SUM = DF_A.sum(axis=1).unstack(TABLE_COLUMNS) + DF_B
 
-    assert np.allclose(df_biomass_table - DF_SUM, 0.)
+    assert np.allclose(df_biomass_table - DF_SUM, 0.0)
+
 
 def test_redistribute_population_table(apportion_combined_biomass_table):
     """
@@ -222,25 +242,25 @@ def test_redistribute_population_table(apportion_combined_biomass_table):
 
     # Create copy for multiple tests
     TEST_DF = apportion_combined_biomass_table.copy()
-    
+
     # Empty exclusion filter
     df_no_filter = apportion.redistribute_population_table(
         population_table=TEST_DF,
         exclusion_filter={},
         group_by=["contrast"],
-    ) 
+    )
 
     # Check equality with `TEST_DF`
     assert df_no_filter.equals(TEST_DF)
     assert all(df_no_filter.index == TEST_DF.index)
     assert sorted(df_no_filter.columns) == sorted(TEST_DF.columns)
-    
+
     # No grouping -- distribute across all contrasts not excluded
     df_no_grouping = apportion.redistribute_population_table(
         population_table=TEST_DF,
         exclusion_filter={"contrast": "A"},
         group_by=[],
-    ) 
+    )
 
     # Check shape and indices with `TEST_DF`
     assert df_no_grouping.shape == TEST_DF.shape
@@ -248,64 +268,64 @@ def test_redistribute_population_table(apportion_combined_biomass_table):
     assert sorted(df_no_grouping.columns) == sorted(TEST_DF.columns)
 
     # Check total sum
-    assert np.isclose(df_no_grouping.sum().sum() - TEST_DF.sum().sum(), 0.)
+    assert np.isclose(df_no_grouping.sum().sum() - TEST_DF.sum().sum(), 0.0)
 
     # Check sliced sums
     assert all(df_no_grouping.sum(axis=1) != TEST_DF.sum(axis=1))
-    assert all(df_no_grouping[[(1, "A"), (2, "A")]] == 0.)  
+    assert all(df_no_grouping[[(1, "A"), (2, "A")]] == 0.0)
 
     # Grouping -- defined by column in exclusion filter
     df_grouping_column = apportion.redistribute_population_table(
         population_table=TEST_DF,
         exclusion_filter={"contrast": "A"},
         group_by=["contrast"],
-    ) 
+    )
 
     # Check shape and indices with `TEST_DF`
     assert df_grouping_column.shape == TEST_DF.shape
     assert all(df_grouping_column.index == TEST_DF.index)
-    assert sorted(df_grouping_column.columns) == sorted(TEST_DF.columns)    
+    assert sorted(df_grouping_column.columns) == sorted(TEST_DF.columns)
 
     # Check total sum
-    assert np.isclose(df_grouping_column.sum().sum() - TEST_DF.sum().sum(), 0.)
+    assert np.isclose(df_grouping_column.sum().sum() - TEST_DF.sum().sum(), 0.0)
 
     # Check sliced sums
-    assert all(df_grouping_column.sum(axis=1) != TEST_DF.sum(axis=1))    
-    assert all(df_grouping_column[[(1, "A"), (2, "A")]] == 0.)  
-    
+    assert all(df_grouping_column.sum(axis=1) != TEST_DF.sum(axis=1))
+    assert all(df_grouping_column[[(1, "A"), (2, "A")]] == 0.0)
+
     # Grouping -- defined by extra bin
     df_grouping_col2 = apportion.redistribute_population_table(
         population_table=TEST_DF,
         exclusion_filter={"extra_bin": 1},
         group_by=["contrast"],
-    ) 
+    )
 
     # Check shape and indices with `TEST_DF`
     assert df_grouping_col2.shape == TEST_DF.shape
     assert all(df_grouping_col2.index == TEST_DF.index)
-    assert sorted(df_grouping_col2.columns) == sorted(TEST_DF.columns)    
+    assert sorted(df_grouping_col2.columns) == sorted(TEST_DF.columns)
 
     # Check total sum
-    assert np.isclose(df_grouping_col2.sum().sum() - TEST_DF.sum().sum(), 0.)
+    assert np.isclose(df_grouping_col2.sum().sum() - TEST_DF.sum().sum(), 0.0)
 
     # Check sliced sums
-    assert all(df_grouping_col2.sum(axis=1) != TEST_DF.sum(axis=1))   
-    assert all(df_grouping_col2[1] == 0.)  
+    assert all(df_grouping_col2.sum(axis=1) != TEST_DF.sum(axis=1))
+    assert all(df_grouping_col2[1] == 0.0)
 
     # Grouping -- defined by index
     df_grouping_index = apportion.redistribute_population_table(
         population_table=TEST_DF,
         exclusion_filter={"index_bin": 5},
         group_by=["contrast"],
-    ) 
+    )
 
     # Check shape and indices with `TEST_DF`
     assert df_grouping_index.shape == TEST_DF.shape
     assert all(df_grouping_index.index == TEST_DF.index)
-    assert sorted(df_grouping_index.columns) == sorted(TEST_DF.columns)    
+    assert sorted(df_grouping_index.columns) == sorted(TEST_DF.columns)
 
     # Check total sum
-    assert np.isclose(df_grouping_index.sum().sum() - TEST_DF.sum().sum(), 0.)
+    assert np.isclose(df_grouping_index.sum().sum() - TEST_DF.sum().sum(), 0.0)
 
     # Check sliced sums
     assert all(df_grouping_index.sum(axis=1) != TEST_DF.sum(axis=1))
@@ -318,5 +338,5 @@ def test_redistribute_population_table(apportion_combined_biomass_table):
             population_table=TEST_DF,
             exclusion_filter={"contrasts": ["A", "B"]},
             group_by=["contrast"],
-        ) 
+        )
     assert len(record) == 1
