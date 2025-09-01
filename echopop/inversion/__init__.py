4aedaedaa2c382f22a8c3a025b7936255a7e53cb
Brandyn Lucca <brandyn.lucca@gmail.com>
2025-08-31 20:04:35 -0700

    Add `InversionMatrix` to `__init__`

--- echopop/inversion/__init__.py
+++ echopop/inversion/__init__.py
@@ -1,11 +1,11 @@
 from .inversion_base import InversionBase
 from .inversion_length_TS import InversionLengthTS
+from .inversion_matrix import InversionMatrix
 from .operations import impute_missing_sigma_bs
-
-# from .inversion_matrix_krill import InversionMatrixKrill
 
 __all__ = [
     "InversionBase",
     "InversionLengthTS",
+    "InversionMatrix",
     "impute_missing_sigma_bs",
 ]
