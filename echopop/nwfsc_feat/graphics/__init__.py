c8a3818e20ec64d01f519209bec221bed33f5ce5
Brandyn Lucca <brandyn.lucca@gmail.com>
2025-09-11 11:13:38 -0700

    Bundle plotting functions into ` __init__` for imports

---
+++ echopop/nwfsc_feat/graphics/__init__.py
@@ -0,0 +1,9 @@
+from .age_length_heatmap import plot_age_length_heatmap
+from .kriged_mesh import plot_kriged_mesh
+from .transect_map import plot_transect_map
+
+__all__ = [
+    "plot_age_length_heatmap",
+    "plot_kriged_mesh",
+    "plot_transect_map",
+]
